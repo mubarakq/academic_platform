bf5e00c54f2d4ae350d9ac796f73111944514359
Adio Mubarak <adiomubarak1999@gmail.com>
2026-02-28 16:59:04 +0100

    setting throttle to app.php and api.php

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Api\AuthController;
 
 Route::get('/test', function () {
     return response()->json([
@@ -15,6 +16,22 @@ Route::controller(AuthController::class)->group(function () {
     Route::post('/login', 'login');
 });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function(){
+
+    Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request){
+        $request->fulfill();
+
+        return response()->json([
+            'message' => 'Email verified successfully.'
+        ]);
+
+    })->middleware(['signed'])->name('verification.verify');
+
+    Route::post('/email/verification-notification', function (Request $request){
+        $request->user()->sendEmailVerificationNotification();
+
+        return response()->json([
+            'message' => 'Verification link sent!'
+        ]);
+    })->middleware('throttle:6,1')->name('verification.send');
 });
